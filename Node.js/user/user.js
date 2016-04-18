b7225873e272c1cbd382fab12792c1b931bbf514
LevKozak <lev@kozak@yahoo.com>
2016-04-18 11:40:28 +0300

    home work for node.js

---
+++ Node.js/user/user.js
@@ -0,0 +1,8 @@
+function User(id,name,company,gender,age) {
+    this.id = id;
+    this.name = name;
+    this.company = company;
+    this.gender = gender;
+    this.age = age;
+};
+module.exports = User;
